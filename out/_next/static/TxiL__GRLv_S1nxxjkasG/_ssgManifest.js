434543c5247de6bf915ea30d88edc64c1a260072
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2022-11-27 07:57:45 +0200

    Deploy gh-pages

--- out/_next/static/TxiL__GRLv_S1nxxjkasG/_ssgManifest.js
+++ out/_next/static/TxiL__GRLv_S1nxxjkasG/_ssgManifest.js
@@ -1,10 +1,10 @@
 self.__SSG_MANIFEST = new Set([
-  '\u002Fblog',
-  '\u002Fabout',
-  '\u002F',
   '\u002Ftags',
-  '\u002Fblog\u002F[...slug]',
+  '\u002Fabout',
+  '\u002Fblog',
+  '\u002F',
   '\u002Fblog\u002Fpage\u002F[page]',
+  '\u002Fblog\u002F[...slug]',
   '\u002Ftags\u002F[tag]',
 ])
 self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB()
