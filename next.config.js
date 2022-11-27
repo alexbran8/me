f8520d90eb1ffdf95ab47b40b46dd63858af0844
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2022-11-27 08:47:17 +0200

    another deploy test

--- next.config.js
+++ next.config.js
@@ -6,12 +6,12 @@ module.exports = {
     loader: 'akamai',
     path: '',
   },
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      // "/about": { page: "/about" },
-    }
-  },
+  // exportPathMap: function () {
+  //   return {
+  //     '/': { page: '/' },
+  //     // "/about": { page: "/about" },
+  //   }
+  // },
   //assetPrefix: '',
   assetPrefix: !debug ? '/' : '',
   webpack: (config, { dev }) => {
