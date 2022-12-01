ea71150d9ad6d63042a487c57dfa86503ebca0c7
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2022-12-01 20:04:11 +0200

    static test #27

--- next.config.js
+++ next.config.js
@@ -58,7 +58,9 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   basePath: process.env.NODE_ENV === 'development' ? '' : '/tailwind-nextjs-blog',
   assetPrefix: process.env.NODE_ENV === 'development' ? '/' : '/tailwind-nextjs-blog/',
-  publicRuntimeConfig: { basePath: process.env.BASE_PATH || '' },
+  publicRuntimeConfig: {
+    basePath: process.env.NODE_ENV === 'development' ? '' : '/tailwind-nextjs-blog',
+  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
