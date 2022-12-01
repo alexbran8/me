ab268c986f4045126ba9718b77d706812178e9fc
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2022-12-01 18:16:08 +0200

    static test #5

--- components/Image.js
+++ components/Image.js
@@ -1,10 +1,6 @@
 import NextImage from 'next/image'
 
 // eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ ...rest }) => {
-  const imageURL = rest.src
-  console.log(typeof imageURL)
-  return <NextImage src={imageURL} width={500} height={1000} alt={'test'} />
-}
+const Image = ({ ...rest }) => <NextImage {...rest} />
 
 export default Image
