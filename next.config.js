fd056d67be37856867ea3a8d16d0af7d08bb1e72
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2022-12-01 19:17:45 +0200

    static test #15

--- next.config.js
+++ next.config.js
@@ -61,6 +61,7 @@ module.exports = withBundleAnalyzer({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    path: process.env.NODE_ENV === 'development' ? '/' : '/tailwind-nextjs-blog/',
     unoptimized: true,
   },
   eslint: {
