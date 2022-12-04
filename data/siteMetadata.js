598951e817d6df65924d86e5f9473ce09065feb8
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2022-12-04 07:17:52 +0200

    basePath update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,9 +7,9 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: `/static/images/logo.png`,
-  image: `/static/images/avatar.png`,
-  socialBanner: `/static/images/twitter-card.png`,
+  siteLogo: `${process.env.BASE_PATH}/static/images/logo.png`,
+  image: `${process.env.BASE_PATH}/static/images/avatar.png`,
+  socialBanner: `${process.env.BASE_PATH}/static/images/twitter-card.png`,
   email: 'address@yoursite.com',
   github: 'https://github.com',
   twitter: 'https://twitter.com/Twitter',
