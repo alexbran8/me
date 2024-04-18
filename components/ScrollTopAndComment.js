9b8dcffa3f389e0bf27dc1e7b43ebffb915ebfd3
Alexandru Bran <BranA>
2024-04-18 12:20:11 +0300

    fix: prettier

--- components/ScrollTopAndComment.js
+++ components/ScrollTopAndComment.js
@@ -21,9 +21,7 @@ const ScrollTopAndComment = () => {
     document.getElementById('comment').scrollIntoView()
   }
   return (
-    <div
-      className={`fixed right-8 bottom-8 hidden flex-col gap-3 ${show ? 'md:flex' : 'md:hidden'}`}
-    >
+    <div className={`right-bottom-8 fixed hidden flex-col gap-3 ${show ? 'md:flex' : 'md:hidden'}`}>
       {siteMetadata.comment.provider && (
         <button
           aria-label="Scroll To Comment"
