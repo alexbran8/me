51b1e20e2bf44f3b6fd46f31dd143361fdf79297
Alexandru Bran <BranA>
2024-04-18 11:50:30 +0300

    feat: disabled/hide newsletter

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react'
 
 import siteMetadata from '@/data/siteMetadata'
 
-const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
+const NewsletterForm = ({ title = 'Subscribe to the newsletter', disabled = true }) => {
   const inputEl = useRef(null)
   const [error, setError] = useState(false)
   const [message, setMessage] = useState('')
@@ -51,16 +51,16 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
             ref={inputEl}
             required
             type="email"
-            disabled={subscribed}
+            disabled={subscribed || disabled}
           />
         </div>
-        <div className="mt-2 flex w-full rounded-md shadow-sm sm:mt-0 sm:ml-3">
+        <div className="mt-2 flex w-full rounded-md shadow-sm sm:ml-3 sm:mt-0">
           <button
-            className={`w-full rounded-md bg-primary-500 py-2 px-4 font-medium text-white sm:py-0 ${
+            className={`w-full rounded-md bg-primary-500 px-4 py-2 font-medium text-white sm:py-0 ${
               subscribed ? 'cursor-default' : 'hover:bg-primary-700 dark:hover:bg-primary-400'
             } focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black`}
             type="submit"
-            disabled={subscribed}
+            disabled={subscribed || disabled}
           >
             {subscribed ? 'Thank you!' : 'Sign up'}
           </button>
